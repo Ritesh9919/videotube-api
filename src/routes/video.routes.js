f5d320591f07cad2e363066248698758485fe524
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-24 10:46:12 +0530

    declared all the controllers and defined router for video controller

---
+++ src/routes/video.routes.js
@@ -0,0 +1,35 @@
+import express from "express";
+const router = express.Router();
+import {
+  getAllVideos,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  publishAVideo,
+  togglePublishStatus,
+} from "../controllers/video.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+router.post(
+  "/",
+  upload.fields([
+    {
+      name: "videoFile",
+      maxCount: 1,
+    },
+    {
+      name: "thumbnain",
+      maxCount: 1,
+    },
+  ]),
+  verifyJwt,
+  publishAVideo
+);
+router.get("/:videoId", verifyJwt, getVideoById);
+router.get("/", getAllVideos);
+router.put("/:videoId", verifyJwt, updateVideo);
+router.delete("/videoId", verifyJwt, deleteVideo);
+router.post("/toggle/publish", verifyJwt, togglePublishStatus);
+
+export default router;
