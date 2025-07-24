7b53b652acd8b159b9051e5bad49bac1008e63f8
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-24 10:17:57 +0530

    Add Like model to handle user interactions with videos, comments, and tweets

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    likedBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    video: {
+      type: mongoose.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: mongoose.Types.ObjectId,
+      ref: "Comment",
+    },
+    tweet: {
+      type: mongoose.Types.ObjectId,
+      ref: "Tweet",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
