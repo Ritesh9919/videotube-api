4dca280e458c7db3a45505611ec46f957e933e95
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-25 11:50:11 +0530

    updated getVideoById controller for adding watched video to watchHistory

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,5 +1,6 @@
 import { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -55,6 +56,10 @@ const getVideoById = asyncHandler(async (req, res) => {
   }
 
   const video = await Video.findById(videoId);
+  const user = await User.findById(req.user._id);
+  user.watchHistory.push(video._id);
+  await user.save();
+
   return res
     .status(200)
     .json(new ApiResponse(200, video, "Video fetched succssfully"));
